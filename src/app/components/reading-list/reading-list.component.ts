a413ad15acb456b8269e0ad31b9e79df0a3c6fa3
ZGApps <appletonzak@gmail.com>
2022-01-21 04:22:40 -0600

    create loopable book accordians in reading list, and outline functions to fill them

--- src/app/components/reading-list/reading-list.component.ts
+++ src/app/components/reading-list/reading-list.component.ts
@@ -1,6 +1,6 @@
 import { BookService } from './../../services/book.service';
 import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/models/GutendexryModels';
+import { Person, User } from 'src/app/models/GutendexryModels';
 
 @Component({
   selector: 'app-reading-list',
@@ -17,9 +17,43 @@ export class ReadingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+
   getUserList():string[]{
     let result: string[] = []
     return result
   }
+
+
+
+  public getBookId(target: number): string{
+    return ''
+  }
+  public getBookTitle(target: number): string{
+    return ''
+  }
+  public getBookSubjects(target: number): string{
+    return ''
+  }
+  public getBookBookshelves(target: number): string{
+    return ''
+  }
+  public getBookCount(target: number): string{
+    return ''
+  }
+  public getBookLanguages(target: number): string{
+    return ''
+  }
+  public getBookAuthors(target: number): string[]{
+    let results: string[] = []
+
+    return []
+    }
+    public getBookTranslators(target: number): string[]{
+      let results: string[] = []
+
+      return results
+      }
+
 
 }
